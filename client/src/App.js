aa1c59f0e7bf4747929e04df9402f0ec244c90e6
Nazmus Ashrafi <nazmus.s.ashrafi@gmail.com>
2022-07-09 00:07:02 +0600

    improved Register page

--- client/src/App.js
+++ client/src/App.js
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
         {/* <Home/> */}
-        <Profile/>
+        {/* <Profile/> */}
         {/* <Login/> */}
-        {/* <Register/> */}
+        <Register/>
     </div>
   );
 }
--- client/src/pages/auth/Register.js
+++ client/src/pages/auth/Register.js
@@ -1,21 +1,65 @@
-import React from 'react'
+
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { motion } from "framer-motion"
 
 const Register = () => {
+
+  const initialState = {
+    firstname: "",
+    lastname: "",
+    username: "",
+    password: "",
+    confirmpass: "",
+  };
+
+  const [data, setData] = useState(initialState);
+  const [confirmPass, setConfirmPass] = useState(true);
+
+  // Reset Form
+  const resetForm = () => {
+    setData(initialState);
+    setConfirmPass(confirmPass);
+  }; // put this in link when we go to login form
+
+  // Handle change in input
+  const handleChange = (e) => {
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+
+  // Form submission
+  const handleSubmit = (e) => {
+
+    setConfirmPass(true);
+    e.preventDefault();
+
+    
+      if(data.password !== data.confirmpass){
+        setConfirmPass(false);
+
+      }
+        
+        
+    
+  };
+
+
+
+
   return (
 
       <div class="text-gray-600 bg-base-100 body-font relative h-screen">
-        <div class="container px-5 py-24 mx-auto  ">
 
-        {/* <motion.div
-          key="modal"
-          initial={{ zoom: 0 }}
-          animate={{ zoom: 1 }}
-          exit={{ zoom: 0 }}
-        > */}
-          {/* <h1 class="font-['Abril'] italic font-bold text-4xl text-sky-500 pb-1 flex flex-col text-center w-full mb-1">Around the world</h1> */}
-        {/* </motion.div> */}
+        <form class="container px-5 py-5 mx-auto infoForm authForm" onSubmit={handleSubmit} >
+
+          {/* <motion.div
+            key="modal"
+            initial={{ zoom: 0 }}
+            animate={{ zoom: 1 }}
+            exit={{ zoom: 0 }}
+          > */}
+            {/* <h1 class="font-['Abril'] italic font-bold text-4xl text-sky-500 pb-1 flex flex-col text-center w-full mb-1">Around the world</h1> */}
+          {/* </motion.div> */}
           
           
           <div class=" bg-base-300 rounded-lg p-8 flex flex-col w-full mt-10 md:mt-0 lg:w-1/2 md:w-2/3 mx-auto">
@@ -24,28 +68,56 @@ const Register = () => {
               <h1 class="font-['Abril'] italic font-bold text-2xl text-cyan-700 pb-1 flex flex-col text-center w-full mb-1">Travelverse</h1>
               <p class="lg:w-2/3 mx-auto leading-relaxed text-zinc-400">It's quick and easy.</p>
             </div>
+
             <div class="relative mb-4">
-              <label for="username" class="leading-7 text-sm text-zinc-400">Name</label>
-              <input type="name" id="name" name="name" class="w-full bg-white rounded border border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+              <label for="username" class="leading-7 text-sm text-zinc-400">First name</label>
+              <input type="name" id="name" name="firstname" class="w-full bg-white rounded border border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleChange} value={data.firstname}/>
             </div>
+
             <div class="relative mb-4">
-              <label for="email" class="leading-7 text-sm text-zinc-400">Email</label>
-              <input type="email" id="email" name="email" class="w-full bg-white rounded border border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+              <label for="username" class="leading-7 text-sm text-zinc-400">Last name</label>
+              <input type="name" id="name" name="lastname" class="w-full bg-white rounded border border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleChange} value={data.lastname}/>
             </div>
+
+            <div class="relative mb-4">
+              <label for="username" class="leading-7 text-sm text-zinc-400">Username</label>
+              <input type="email" id="username" name="username" class="w-full bg-white rounded border border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleChange} value={data.username}/>
+            </div>
+
             <div class="relative mb-4">
               <label for="password" class="leading-7 text-sm text-zinc-400">Password</label>
-              <input type="password" id="password" name="password" class="w-full bg-white rounded border border-gray-300 focus:border-purple-500 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
+              <input type="password" id="password" name="password" class="w-full bg-white rounded border border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleChange} value={data.password}/>
             </div>
-            <button class="text-white bg-cyan-700 hover:bg-cyan-800 border-0 py-2 px-8 focus:outline-none rounded text-lg">Sign Up</button>
 
-            {/* <Link class="text-s text-purple-400 hover:text-purple-500 mt-3 cursor-pointer m-auto" to="/">
+            <div class="relative mb-4">
+              <label for="confirmpassword" class="leading-7 text-sm text-zinc-400">Confirm password</label>
+              <input type="password" id="password" name="confirmpass" class="w-full bg-white rounded border border-gray-300 focus:border-cyan-600 focus:ring-2 focus:ring-purple-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out" onChange={handleChange} value={data.confirmpass}/>
+            </div>
+
+            <span
+            style={{
+              color: "red",
+              fontSize: "12px",
+              alignSelf: "flex-end",
+              marginRight: "5px",
+              display: confirmPass ? "none" : "block",
+            }}
+            >
+              *Confirm password is not same
+            </span>
+
+
+            <button class="text-white bg-cyan-700 hover:bg-cyan-800 border-0 py-2 px-8 focus:outline-none rounded text-lg mt-5" type="submit">Sign Up</button>
+
+            <button class="text-s text-purple-400 hover:text-purple-500 mt-3 cursor-pointer m-auto" to="/">
               Back to login
 
-            </Link> */}
+            </button> 
+            {/* link */}
           
           </div>
           
-        </div>
+        </form>
       </div>
 
   )
